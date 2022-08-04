f66b49b953dc3c48d9169c147bd0a9b95f40d42f
RomelClavel <romeletoty@gmail.com>
2022-08-04 16:50:36 +0200

    refactor: added structure folders and ordered components in them

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,20 @@
+import Header from '../layout/Header';
+import LoginComponent from './LoginComponent';
+
+function LoginPage({ setIsAuthenticated }) {
+	return (
+		<div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+			<div className="max-w-md w-full space-y-8">
+				<Header
+					heading="Login to your account"
+					paragraph="Don't have an account yet? "
+					linkName="Signup"
+					linkUrl="/signup"
+				/>
+				<LoginComponent setIsAuthenticated={setIsAuthenticated} />
+			</div>
+		</div>
+	);
+}
+
+export default LoginPage;

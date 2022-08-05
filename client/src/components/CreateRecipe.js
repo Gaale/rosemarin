460fd041e0f21336c43dcbd0e714eb7c5305e1a6
Keval-P21 <keval.p21+github@gmail.com>
2022-08-05 17:10:16 +0100

    Create recipe js refactor

--- client/src/components/CreateRecipe.js
+++ client/src/components/CreateRecipe.js
@@ -1,288 +1,41 @@
-// import * as React from 'react';
-// import { useRef, useState } from 'react';
-// import TopSection from './TopSection';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import Instruction from './Instruction';
-// import Ingredient from './Ingredient';
-// import { postRecipe } from '../Utils/apiDBService';
-
-// interface SubInstructionType {text: string}
-// interface TmpIngredientsType {name: string, quantity: string, unit?: string}
-
-// function CreateRecipe() {
-//   const [ingredients, setIngredients] = useState(['1-ingredient']);
-//   const [instructions, setInstructions] = useState(['1-instruction']);
-
-//   const form = useRef<HTMLFormElement>(null);
-//   // const form = useRef(null);
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     // let subInstructions = [];
-//     let subInstructions:SubInstructionType[] = [];
-//     // let tmpIngredients = [];
-//     // let tmpIngredients:String[] = [];
-//     let tmpIngredients:TmpIngredientsType[] = [];
-//     // const formData = new FormData(form.current);
-//     const formData = new FormData(form.current!);
-
-//     for (let [key, value] of formData.entries()) {
-//       if (key.includes('instruction') && typeof value == 'string') subInstructions.push({ text: value });
-//       // if (key.includes('instruction')) subInstructions.push( value);
-//     }
-
-//     const map = new Map(formData.entries());
-//     map.forEach((value, key) => {
-//       if (!key.includes('ingredient')) map.delete(key);
-//     });
-//     console.log(map);
-
-//     const tmpMap = new Map();
-//     // const tmpMap = new Map<string, string>();
-//     for (let i = 0; i < map.size; i++) {
-//       map.forEach((value, key) => {
-//         if (key.includes(`${i}`)) tmpMap.set(key.split('-')[0], value);
-//       });
-//       tmpIngredients.push(Object.fromEntries(tmpMap));
-//     }
-
-//     tmpIngredients = tmpIngredients.reduce((o, i) => {
-//       if (!o.find((v:TmpIngredientsType) => v.name == i.name)) {
-//         o.push(i);
-//       }
-//       return o;
-//     }, []);
-
-//     tmpIngredients.shift();
-
-//     const newRecipe = {
-//       title: e.target.title.value,
-//       description: e.target.description.value,
-//       img_url: e.target.url.value || null,
-//       // files: e.target[7].files[0] || null,
-//       img_alt_text: e.target.title.value,
-//       ingredients: tmpIngredients,
-//       instructions: subInstructions,
-//     };
-//     postRecipe(newRecipe)
-//       .then((res) => console.log(res))
-//       .catch((error) => console.log(error));
-
-//     e.target.title.value = '';
-//     e.target.description.value = '';
-//     e.target.url.value = '';
-//     e.target.title = '';
-//   };
-
-//   const addHandlerInstruction = () => {
-//     setInstructions((prev) => {
-//       let count = parseInt(prev[prev.length - 1], 10);
-//       return [...prev, count + 1 + '-instruction'];
-//     });
-//   };
-
-//   const delHandlerInstruction = () => {
-//     setInstructions((prev) => {
-//       let count = parseInt(prev[prev.length - 1], 10);
-//       if (count < 2) count = 100;
-//       const filtered = prev.filter((instr) => parseInt(instr) !== count);
-//       return [...filtered];
-//     });
-//   };
-
-//   const addHandlerIngredient = () => {
-//     setIngredients((prev) => {
-//       let count = parseInt(prev[prev.length - 1], 10);
-//       return [...prev, count + 1 + '-ingredient'];
-//     });
-//   };
-
-//   const delHandlerIngredient = () => {
-//     setIngredients((prev) => {
-//       let count = parseInt(prev[prev.length - 1], 10);
-//       if (count < 2) count = 100;
-//       const filtered = prev.filter((ingr) => parseInt(ingr) !== count);
-//       return [...filtered];
-//     });
-//   };
-
-//   return (
-//     <>
-//       <TopSection />
-//       <form
-//         ref={form}
-//         encType='multipart/form-data'
-//         onSubmit={handleSubmit}
-//         className='w-2/3 m-auto form-control prose lg:prose-xl mb-40'
-//       >
-//         <h2 className='m-auto font-rufina-bold'>Create your own recipe</h2>
-//         <div>
-//           <label className='label'>Title</label>
-//           <input
-//             type='text'
-//             name='title'
-//             placeholder='Type here title of your recipe...'
-//             className='input input-bordered w-full hover:bg-slate-50'
-//           />
-//         </div>
-
-//         <div>
-//           <label className='label'>Description</label>
-//           <textarea
-//             name='description'
-//             placeholder='Type here description of your recipe...'
-//             className='textarea input-bordered w-full hover:bg-slate-50 cursor-pointer'
-//           />
-//         </div>
-
-//         <div>
-//           <label className='label justify-start mr-10'>
-//             Ingredients
-//             <FontAwesomeIcon
-//               icon='fa-solid fa-plus'
-//               className='text-warning transition-all hover:text-2xl ml-10'
-//               onClick={addHandlerIngredient}
-//             />
-//             <FontAwesomeIcon
-//               icon='fa-solid fa-minus'
-//               className='text-warning transition-all hover:text-2xl cursor-pointer ml-10'
-//               onClick={delHandlerIngredient}
-//             />
-//           </label>
-//           {ingredients.map((ingredient) => {
-//             return <Ingredient ingredient={ingredient} key={ingredient} />;
-//           })}
-//         </div>
-
-//         <div>
-//           <label className='label justify-start mr-10'>
-//             Instructions
-//             <FontAwesomeIcon
-//               icon='fa-solid fa-plus'
-//               className='text-warning transition-all hover:text-2xl cursor-pointer ml-10'
-//               onClick={addHandlerInstruction}
-//             />
-//             <FontAwesomeIcon
-//               icon='fa-solid fa-minus'
-//               className='text-warning transition-all hover:text-2xl cursor-pointer ml-10'
-//               onClick={delHandlerInstruction}
-//             />
-//           </label>
-//           {instructions.map((instruction) => {
-//             return <Instruction instruction={instruction} key={instruction} />;
-//           })}
-//         </div>
-
-//         <div>
-//           <label className='label'>URL of image</label>
-//           <input
-//             type='input'
-//             name='url'
-//             placeholder='Type here URL if needed..'
-//             className='input input-bordered w-full hover:bg-slate-50'
-//           />
-//         </div>
-
-//         <div>
-//           <label className='label'>Upload image</label>
-//           <input
-//             type='file'
-//             name='file'
-//             className='block w-full text-sm text-slate-500
-//                        file:mr-4 file:py-2 file:px-4
-//                        file:square file:border-0
-//                        file:text-sm file:font-semibold
-//                        file:bg-fuchsia-50 file:text-accent-700
-//                        hover:file:bg-base-300 mb-10'
-//           />
-//         </div>
-
-//         <button type='submit' className='btn btn-neutral font-rufina-regular'>
-//           Submit
-//         </button>
-//       </form>
-//     </>
-//   );
-// }
-
-// export default CreateRecipe;
-
 import React, { useRef, useState } from 'react';
 import TopSection from './TopSection';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Instruction from './Instruction';
-import Ingredient from './Ingredient';
+// import Instruction from './Instruction';
+// import Ingredient from './Ingredient';
 import { postRecipe } from '../Utils/apiDBService';
 
 function CreateRecipe() {
-  const [ingredients, setIngredients] = useState(['1-ingredient']);
-  const [instructions, setInstructions] = useState(['1-instruction']);
-
-  const form = useRef(null);
+  const [ingredients, setIngredients] = useState([
+    { name: '', quantity: 0, unit: '' },
+  ]);
+  const [instructions, setInstructions] = useState(['']);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    let subInstructions = [];
-    let tmpIngredients = [];
-    const formData = new FormData(form.current);
 
-    for (let [key, value] of formData.entries()) {
-      if (key.includes('instruction')) subInstructions.push({ text: value });
-    }
-
-    const map = new Map(formData.entries());
-    map.forEach((value, key) => {
-      if (!key.includes('ingredient')) map.delete(key);
-    });
-
-    const tmpMap = new Map();
-    for (let i = 0; i < map.size; i++) {
-      map.forEach((value, key) => {
-        if (key.includes(`${i}`)) tmpMap.set(key.split('-')[0], value);
-      });
-      tmpIngredients.push(Object.fromEntries(tmpMap));
-    }
-
-    // tmpIngredients = tmpIngredients.reduce((o, i) => {
-    //   console.log({ o, i });
-    //   if (
-    //     !o.find((v) => {
-    //       console.log({ v });
-    //       return v.name == i.name;
-    //     })
-    //   ) {
-    //     o.push(i);
-    //   }
-    //   console.log({ o });
-    //   return o;
-    // }, []);
-
-    tmpIngredients = tmpIngredients.reduce((o, i) => {
-      console.log({ o, i });
-      if (
-        !o.find((v) => {
-          console.log({ v });
-          return v.name == i.name;
-        })
-      ) {
-        o.push(i);
+    const newTmpIngredients = ingredients.reduce((ingreds, ing) => {
+      const itemIndex = ingreds.findIndex((item) => item.name === ing.name);
+      if (itemIndex === -1) {
+        ingreds = [...ingreds, ing];
+      } else {
+        ingreds[itemIndex].quantity = (
+          Number(ingreds[itemIndex].quantity) + Number(ing.quantity)
+        ).toString();
       }
-      console.log({ o });
-      return o;
+      return ingreds;
     }, []);
 
-    tmpIngredients.shift();
-
-    const newRecipe = {
+    const data = {
       title: e.target.title.value,
       description: e.target.description.value,
       img_url: e.target.url.value || null,
       // files: e.target[7].files[0] || null,
-      img_alt_text: e.target.title.value,
-      ingredients: tmpIngredients,
-      instructions: subInstructions,
+      img_alt_text: e.target.title.value || null,
+      ingredients: newTmpIngredients,
+      instructions: instructions,
     };
-    postRecipe(newRecipe)
+    postRecipe(data)
       .then((res) => console.log(res))
       .catch((error) => console.log(error));
 
@@ -292,43 +45,55 @@ function CreateRecipe() {
     e.target.title = '';
   };
 
-  const addHandlerInstruction = () => {
-    setInstructions((prev) => {
-      let count = parseInt(prev[prev.length - 1], 10);
-      return [...prev, count + 1 + '-instruction'];
-    });
-  };
-
-  const delHandlerInstruction = () => {
-    setInstructions((prev) => {
-      let count = parseInt(prev[prev.length - 1], 10);
-      if (count < 2) count = 100;
-      const filtered = prev.filter((instr) => parseInt(instr) !== count);
-      return [...filtered];
-    });
-  };
-
   const addHandlerIngredient = () => {
-    setIngredients((prev) => {
-      let count = parseInt(prev[prev.length - 1], 10);
-      return [...prev, count + 1 + '-ingredient'];
-    });
+    setIngredients([...ingredients, { name: '', quantity: 0, unit: '' }]);
   };
 
-  const delHandlerIngredient = () => {
-    setIngredients((prev) => {
-      let count = parseInt(prev[prev.length - 1], 10);
-      if (count < 2) count = 100;
-      const filtered = prev.filter((ingr) => parseInt(ingr) !== count);
-      return [...filtered];
-    });
+  const delHandlerIngredient = (index) => {
+    if (ingredients.length === 1) return;
+    const rows = [...ingredients];
+    rows.splice(index, 1);
+    setIngredients(rows);
+  };
+
+  const addHandlerInstruction = () => {
+    setInstructions([...instructions, '']);
+  };
+
+  const delHandlerInstruction = (index) => {
+    if (instructions.length === 1) return;
+    const rows = [...instructions];
+    rows.splice(index, 1);
+    setInstructions(rows);
+  };
+
+  const handleChange = (index, event) => {
+    const { name, value } = event.target;
+    const list = [...ingredients];
+    list[index][name] = value;
+    setIngredients(list);
+  };
+
+  const handleChangeQuantity = (index, event) => {
+    const { name, value } = event.target;
+    const list = [...ingredients];
+    if (list[index][name]) list[index][name] += value;
+    list[index][name] = value;
+    setIngredients(list);
+  };
+
+  const handleChangeInstructions = (index, event) => {
+    const { value } = event.target;
+    const list = [...instructions];
+    list[index] = value;
+    setInstructions(list);
   };
 
   return (
     <>
       <TopSection></TopSection>
       <form
-        ref={form}
+        // ref={form}
         encType='multipart/form-data'
         onSubmit={handleSubmit}
         className='w-2/3 m-auto form-control prose lg:prose-xl mb-40'
@@ -367,8 +132,39 @@ function CreateRecipe() {
               onClick={delHandlerIngredient}
             />
           </label>
-          {ingredients.map((ingredient) => {
-            return <Ingredient ingredient={ingredient} key={ingredient} />;
+          {ingredients.map((data, i) => {
+            const { name, quantity, unit } = data;
+            return (
+              <div key={i} className='flex justify-between mb-3'>
+                <input
+                  type='text'
+                  id='name'
+                  name='name'
+                  value={name}
+                  placeholder='Type here ingredient..'
+                  className='input input-bordered w-1/3 hover:bg-slate-50'
+                  onChange={(event) => handleChange(i, event)}
+                />
+                <input
+                  type='number'
+                  id='quantity'
+                  value={quantity}
+                  name='quantity'
+                  placeholder='quantity..'
+                  className='input input-bordered w-1/4 mr-3 mr-3 hover:bg-slate-50'
+                  onChange={(event) => handleChange(i, event)}
+                />
+                <input
+                  type='text'
+                  id='unit'
+                  value={unit}
+                  name='unit'
+                  placeholder='unit..'
+                  className='input input-bordered w-1/3 hover:bg-slate-50'
+                  onChange={(event) => handleChange(i, event)}
+                />
+              </div>
+            );
           })}
         </div>
 
@@ -386,8 +182,18 @@ function CreateRecipe() {
               onClick={delHandlerInstruction}
             />
           </label>
-          {instructions.map((instruction) => {
-            return <Instruction instruction={instruction} key={instruction} />;
+          {instructions.map((data, i) => {
+            return (
+              <textarea
+                key={i}
+                name='instruction'
+                id='instruction'
+                value={data}
+                placeholder='Type here instruction..'
+                className='textarea input-bordered w-full hover:bg-slate-50'
+                onChange={(event) => handleChangeInstructions(i, event)}
+              />
+            );
           })}
         </div>
 

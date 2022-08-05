9c06c9b06b556865f606f2805385fbc45ef8dbae
Alejandro Fuchs <el.sandor.fuchs@gmail.com>
2022-08-05 18:17:50 +0200

    migrate be middlewares to TS

---
+++ server/middlewares/uploadFileMiddleware.ts
@@ -0,0 +1,31 @@
+import { Response, Request, NextFunction } from 'express';
+
+import path from 'path';
+import { RecipeType } from '../types/Recipe';
+
+const fileMiddleware = async (
+  req: Request<any, any, RecipeType>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    if (req.files) {
+      console.log('Llega aqui?');
+      let file = req.files.file;
+      if (!Array.isArray(file)) {
+        let ext = path.extname(file.name);
+        let newName = file.md5 + ext;
+        await file.mv('./images/' + newName);
+        req.body.image = path.relative('/', `../images/${newName}`);
+        next();
+      }
+    } else {
+      next();
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('some error with file');
+  }
+};
+
+module.exports = fileMiddleware;

cfd7d01544f0f6e3367f209dc23deef1c9386134
Alejandro Fuchs <el.sandor.fuchs@gmail.com>
2022-08-06 09:45:42 +0200

    add be test for userController

--- server/test/userController.test.js
+++ server/test/userController.test.js
@@ -3,34 +3,40 @@ const request = require('supertest');
 const baseUrl = 'http://localhost:3001';
 
 const mockUser = {
-	name: 'string',
-	email: 'string@string.com',
-	password: 'string',
+  name: 'string',
+  email: 'string@string.com',
+  password: 'string',
 };
 
 let agent;
 describe('User endpoint', () => {
-	it('should create a new user & return a 201 status code', async () => {
-		const response = await request(baseUrl).post('/login').send(mockUser);
-		expect(response.statusCode).toBe(200);
-		agent = response;
-		expect(response.text).toEqual('Ok');
-	});
+  it('should create a new user & return a 201 status code', async () => {
+    const response = await request(baseUrl).post('/login').send(mockUser);
+    expect(response.statusCode).toBe(200);
+    agent = response;
+    expect(response.text).toEqual('Ok');
+  });
 
-	it('should get user & return a 200 status code', async () => {
-		const response = await request(baseUrl)
-			.get('/me')
-			.set('Cookie', [`${agent.header['set-cookie'][0]}`]);
+  it('should get user & return a 200 status code', async () => {
+    const response = await request(baseUrl)
+      .get('/me')
+      .set('Cookie', [`${agent.header['set-cookie'][0]}`]);
 
-		expect(response.statusCode).toBe(200);
-		expect(response.body).toHaveProperty('name');
-	});
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty('name');
+    expect(response.body).toHaveProperty('email');
+  });
+
+  it('should logout & return a 200 status code', async () => {
+    const response = await request(baseUrl)
+      .get('/logout')
+      .set('Cookie', [`${agent.header['set-cookie'][0]}`]);
+
+    console.log('////////////////////////////////    ', response.header);
+    expect(
+      response.header['set-cookie'] === agent.header['set-cookie']
+    ).toBeFalsy();
+
+    expect(response.statusCode).toBe(200);
+  });
 });
-
-// request(url)
-// 	.post('/some/place')
-// 	.set('Accept-Language', 'en')
-// 	.set('Cookie', ['myApp-token=12345667', 'myApp-other=blah'])
-// 	.end(function (err, result) {
-// 		// test the result
-// 	});
